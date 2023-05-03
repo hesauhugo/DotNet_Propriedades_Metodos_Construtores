cceeb0629715bd1d36b3768c935878af3f9df677
hesauhugo <hesau.hugo@hotmail.com>
2023-05-03 20:01:21 -0300

    feat: projeto criado

---
+++ Pessoa.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading.Tasks;
+
+namespace Explorando.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+    }
+}
